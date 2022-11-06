33fd5b06388d2fe04ec9f141d432c5eb6490c327
marvin evins <marvinevins@yahoo.com>
2022-11-05 19:54:43 -0500

    working on header css

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import SearchIcon from '@mui/icons-material/Search';
 function Header() {
   return (
     <div className='header'>
-        <h1> this is the header</h1>
+        {/* <h1> this is the header</h1> */}
         <div className="header__left">
                 <img src="https://www.flaticon.com/free-icon/linkedin_3536505" alt="" />
                 <div className="header__search">
