066314c89484b5450736844ba11725babe08aad8
marvin evins <marvinevins@yahoo.com>
2022-11-05 19:10:22 -0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBWBvf8v2EK-gWo6hcJFCYYX7dOo4gNqCE",
+    authDomain: "linkedin-clone-1d8df.firebaseapp.com",
+    projectId: "linkedin-clone-1d8df",
+    storageBucket: "linkedin-clone-1d8df.appspot.com",
+    messagingSenderId: "37948022128",
+    appId: "1:37948022128:web:0de9f28c54eecbc58ec3b5",
+    measurementId: "G-LR964D3P4C"
+  };
