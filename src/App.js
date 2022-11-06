2256468df8a5d2678f511e2bc51138ba13504345
marvin evins <marvinevins@yahoo.com>
2022-11-05 19:45:18 -0500

    working on header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
   return (
     <div className="app">
       <h1>lets build a linked in clone</h1>
+
+      {/* header */}
+
+      {/* app body */}
+         {/* sidebar */}
+         {/* feed */}
+         {/* widgets */}
+
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+  return (
+    <div className='header'>
+        <h1> this is the header</h1>
+        <div className="header__left">
+                <img src="https://www.flaticon.com/free-icon/linkedin_3536505" alt="" />
+                <div className="header__search">
+                    {/* seARCH ICON */}
+                    <SearchIcon />
+                    <input type="text" />
+
+
+                </div>
+        </div>
+
+        <div className="header__right">
+
+        </div>
+        </div>
+  )
+}
+
+export default Header
